90dc26d80647ce84170f4d4d4fb6cb980faee20c
Sourav <souravghosh251@outlook.com>
2026-01-12 19:15:23 +0530

    pattern 2

--- patterns/01.py
+++ patterns/01.py
@@ -1,6 +1,7 @@
 n = int(input("Enter a number"))
 
-for i in range(0,n):
-    for j in range(0,n):
-        print("*", end=" ")
+for i in range(n):
+    for j in range(n):
+        if(i<=j):
+            print("*", end=" ")
     print()
