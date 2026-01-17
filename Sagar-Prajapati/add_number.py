33a547a6368b0ca069c676b1cd22d2fe43ae7e8d
Sourav <souravghosh251@outlook.com>
2026-01-17 20:49:52 +0530

    three approaches

--- Sagar-Prajapati/add_number.py
+++ Sagar-Prajapati/add_number.py
@@ -1,4 +1,17 @@
+#Brute Force Approach
 
+def add_num_brut(lst):
+    n=len(lst)
+    for i in range(0,n):
+        for j in range(i+1,n):
+            if lst[i]+lst[j]==0:
+                return True
+    return False
+
+
+
+
+# Hash Set Approach
 def add_num(lst):
     if len(lst)<2:
         return False
@@ -7,5 +20,23 @@ def add_num(lst):
         if -i in l1:
             return True
     return False
+#lst = [1,2,3,4,-1,-2,-3,-4]
+#print(add_num_brut(lst))
+
+# Two Pointer Approach
+
+def add_num_two_pointer(lst):
+    lst.sort()
+    left,right=0,len(lst)-1
+    while left < right:
+        s= lst[left]+ lst[right]
+        if s==0:
+            return True
+        elif s<0:
+            left+=1
+        else:
+            right+=-1
+    return False
+
 lst = [1,2,3,4,-1,-2,-3,-4]
-print(add_num(lst))
+print(add_num_two_pointer(lst))
